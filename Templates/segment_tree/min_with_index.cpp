26c8afaee27662cd5db1685abb63becab99618ed
Robin Singh <30548236+genius-noob@users.noreply.github.com>
2020-05-10 03:39:54 +0530

    Update min_with_index.cpp

--- Templates/segment_tree/min_with_index.cpp
+++ Templates/segment_tree/min_with_index.cpp
@@ -10,7 +10,7 @@ node merge(node &a, node &b) {
 void build(int idx, int start, int end) {
     if(start == end) {
         // tree[idx].value = a[start]; tree[idx].index = start;
-        tree[idx] = {a[start], start}
+        tree[idx] = {a[start], start};
     }
     else {
         int mid = start + end >> 1;
@@ -19,7 +19,24 @@ void build(int idx, int start, int end) {
         tree[idx] = merge(tree[idx << 1], tree[idx << 1 | 1]);
     }
 }
- 
+
+void update(int idx, int start, int end, int pos, int value) {
+    if(start == end) {
+        tree[idx] = {value, start};
+        a[pos] = value;
+    }
+    else {
+        int mid = start + end >> 1;
+        if(pos <= mid) update(idx << 1, start, mid, pos, value);
+        else update(idx << 1 | 1, mid+1, end, pos, value);
+        tree[idx] = merge(tree[idx << 1], tree[idx << 1 | 1]);
+    }
+}
+
+void update(int pos, int value) {
+    update(1, 0, n-1, pos, value);
+}
+
 node query(int idx, int start, int end, int ql, int qr) {
     if(start > qr || end < ql) return {inf, inf};
     if(start >= ql && end <= qr) return tree[idx];
